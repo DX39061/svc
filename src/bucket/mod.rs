d4caf79f3425f869bdbd142c314add91e45f02b7
dx3906 <21009201351@stu.xidian.edu.cn>
2023-02-04 11:31:28 +0800

    impl repo init & info

--- src/bucket/mod.rs
+++ src/bucket/mod.rs
@@ -1,17 +1,71 @@
+use repo::{check_svc_repo, RepoMeta};
+use std::{env, fs, io, process};
+mod repo;
+mod storage;
+
 pub fn info() {
-    println!("command info")
+    // println!("command info");
+    match check_svc_repo() {
+        Ok(svc_path) => {
+            let repo_meta = RepoMeta::read_repo_meta(svc_path);
+            println!("-----------------------------------------");
+            println!("{}", repo_meta);
+            println!("-----------------------------------------");
+            println!("notice: you can edit .svc/repo to set metadata mannually.")
+        }
+        Err(err) => {
+            eprintln!("{}", err);
+            process::exit(1)
+        }
+    }
 }
 
 pub fn init() {
-    println!("command init")
+    println!("command init");
+    match check_svc_repo() {
+        Ok(_) => {
+            eprintln!("error: svc repo already exists!");
+            process::exit(1)
+        }
+        Err(_) => {
+            println!("-----------------------------------------");
+            let current_dir = env::current_dir().unwrap();
+            fs::create_dir(current_dir.join(".svc")).unwrap();
+            println!("notice: create .svc directory successfully.");
+            let file = fs::File::create(current_dir.join(".svc/repo")).unwrap();
+            drop(file);
+            println!("notice: create .svc/repo successfully.");
+            let mut repo_name = String::from("");
+            let mut repo_intro = String::from("");
+            let mut remote_url = String::from("");
+            println!("-----------------------------------------");
+            println!("now you could set some metadata for this repo, or just type 'enter' to ignore them.");
+            println!("repo_name: ");
+            io::stdin().read_line(&mut repo_name).unwrap();
+            println!("repo_intro: ");
+            io::stdin().read_line(&mut repo_intro).unwrap();
+            println!("remote_url: ");
+            io::stdin().read_line(&mut remote_url).unwrap();
+            println!("-----------------------------------------");
+            let new_meta = RepoMeta::new(repo_name, repo_intro, remote_url);
+            match RepoMeta::update_repo_meta(new_meta, current_dir.join(".svc")) {
+                Ok(()) => {
+                    println!("notice: svc repo initialize completely");
+                }
+                Err(err) => {
+                    eprintln!("error: {}", err);
+                }
+            }
+        }
+    }
 }
 
 pub fn log() {
-    println!("commad log")
+    println!("command log")
 }
 
 pub fn commit() {
-    println!("commad commit")
+    println!("command commit")
 }
 
 pub fn checkout(version: &String) {
---
+++ src/bucket/repo.rs
@@ -0,0 +1,138 @@
+use chrono::Local;
+use std::{
+    fmt::Display,
+    fs::File,
+    io::{BufRead, BufReader, Error, Write},
+    path::PathBuf,
+};
+pub struct RepoMeta {
+    repo_name: String,
+    repo_intro: String,
+    remote_url: String,
+    created_at: String,
+    updated_at: String,
+}
+
+impl Display for RepoMeta {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(
+            f,
+            "repo_name = {}\nrepo_intro = {}\nremote_url = {}\ncreated_at = {}\nupdated_at = {}",
+            self.repo_name, self.repo_intro, self.remote_url, self.created_at, self.updated_at
+        )
+    }
+}
+
+impl RepoMeta {
+    pub fn new(repo_name: String, repo_intro: String, remote_url: String) -> RepoMeta {
+        let created_at = Local::now().format("%Y-%m-%d %H:%M").to_string();
+        let updated_at = created_at.clone();
+
+        // println!("{:?}", created_at);
+        RepoMeta {
+            repo_name,
+            repo_intro,
+            remote_url,
+            created_at,
+            updated_at,
+        }
+    }
+
+    pub fn read_repo_meta(svc_path: PathBuf) -> RepoMeta {
+        // println!("{:?}", svc_path.join("repo"));
+        let file =
+            File::open(svc_path.join("repo")).expect("can not read .svc/repo, maybe it lost?");
+        let file = BufReader::new(file);
+        let mut repo_name = String::from("");
+        let mut repo_intro = String::from("");
+        let mut remote_url = String::from("");
+        let mut created_at = String::from("");
+        let mut updated_at = String::from("");
+        for line in file.lines() {
+            let line = line.unwrap();
+            let line: Vec<&str> = line.split('=').collect();
+            if line.len() != 2 {
+                continue;
+            }
+            let k = line[0].trim();
+            let v = line[1].trim().to_string();
+            match k {
+                "repo_name" => {
+                    repo_name = v;
+                }
+                "repo_intro" => {
+                    repo_intro = v;
+                }
+                "remote_url" => {
+                    remote_url = v;
+                }
+                "created_at" => {
+                    created_at = v;
+                }
+                "updated_at" => {
+                    updated_at = v;
+                }
+                _ => (),
+            }
+        }
+        RepoMeta {
+            repo_name,
+            repo_intro,
+            remote_url,
+            created_at,
+            updated_at,
+        }
+    }
+
+    pub fn update_repo_meta(new_meta: RepoMeta, svc_path: PathBuf) -> Result<(), Error> {
+        let old_meta = RepoMeta::read_repo_meta(svc_path.clone());
+        let mut repo_name = old_meta.repo_name;
+        let mut repo_intro = old_meta.repo_intro;
+        let mut remote_url = old_meta.remote_url;
+        let mut created_at = old_meta.created_at;
+        let mut updated_at = old_meta.updated_at;
+        if new_meta.repo_name != "\n" {
+            repo_name = new_meta.repo_name;
+        }
+        if new_meta.repo_intro != "\n" {
+            repo_intro = new_meta.repo_intro;
+        }
+        if new_meta.remote_url != "\n" {
+            remote_url = new_meta.remote_url;
+        }
+        if new_meta.created_at != "\n" {
+            created_at = new_meta.created_at;
+        }
+        if new_meta.updated_at != "\n" {
+            updated_at = new_meta.updated_at;
+        }
+        // println!("{:?}", svc_path.join("repo"));
+        let mut file =
+            File::create(svc_path.join("repo")).expect("can not open .svc/repo, maybe it lost?");
+        
+        file.write_fmt(format_args!("repo_name = {}\n", repo_name))?;
+        file.write_fmt(format_args!("repo_intro = {}\n", repo_intro))?;
+        file.write_fmt(format_args!("remote_url = {}\n", remote_url))?;
+        file.write_fmt(format_args!("created_at = {}\n", created_at))?;
+        file.write_fmt(format_args!("updated_at = {}\n", updated_at))?;
+
+        Ok(())
+    }
+}
+
+pub fn check_svc_repo() -> Result<PathBuf, &'static str> {
+    let mut path = std::env::current_dir().unwrap();
+    // println!("{:?}", path);
+    loop {
+        let svc_path = path.join(".svc");
+        if let Ok(true) = svc_path.try_exists() {
+            return Ok(svc_path);
+        }
+        if let Some(parent) = path.parent() {
+            // println!("{:?}", parent);
+            path = parent.to_path_buf();
+        } else {
+            return Err("not a svc repo yet");
+        }
+    }
+}
